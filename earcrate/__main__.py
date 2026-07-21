d2de364a608ae39d13c70f7399bc68e17224d4ca
BigBirdReturns <bigbirdreturns@proton.me>
2026-07-20 20:22:03 -0700

    cli: route the MIDI command surface

--- earcrate/__main__.py
+++ earcrate/__main__.py
@@ -1,3 +1,10 @@
 import sys
+
+if len(sys.argv) > 1 and sys.argv[1] == "midi":
+    from earcrate.midi.cli import midi_main
+
+    sys.exit(midi_main(sys.argv[2:]))
+
 from earcrate.cli import main
+
 sys.exit(main())

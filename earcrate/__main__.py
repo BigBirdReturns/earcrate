9a0c3dd2a8141a02c7ada504fa3caf16fbedc156
BigBirdReturns <bigbirdreturns@proton.me>
2026-07-21 10:00:01 -0700

    Route live and reference commands

--- earcrate/__main__.py
+++ earcrate/__main__.py
@@ -5,6 +5,16 @@ if len(sys.argv) > 1 and sys.argv[1] == "midi":
 
     sys.exit(midi_main(sys.argv[2:]))
 
+if len(sys.argv) > 1 and sys.argv[1] == "live":
+    from earcrate.live.cli import live_cli_main
+
+    sys.exit(live_cli_main(sys.argv[2:]))
+
+if len(sys.argv) > 1 and sys.argv[1] == "reference":
+    from earcrate.study.reference_cli import reference_cli_main
+
+    sys.exit(reference_cli_main(sys.argv[2:]))
+
 from earcrate.cli import main
 
 sys.exit(main())

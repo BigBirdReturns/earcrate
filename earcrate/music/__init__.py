860a65dae8ba7cb9abc0f5d223499b0c181f4670
BigBirdReturns <bigbirdreturns@proton.me>
2026-07-21 23:02:15 -0700

    Start the player-piano musical authority package

---
+++ earcrate/music/__init__.py
@@ -0,0 +1,7 @@
+"""EarCrate musical authority: laws, equations, obligations, and player pianos."""
+
+from .model import *
+from .equations import *
+from .laws import *
+from .player_piano import *
+from .heritage import *

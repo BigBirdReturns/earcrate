2eddb059b84a03c27ea70ec780a430c415e1543e
BigBirdReturns <bigbirdreturns@proton.me>
2026-07-23 23:30:19 -0700

    Expose cephalopod song-reader package

---
+++ earcrate/reader/__init__.py
@@ -0,0 +1,9 @@
+"""EarCrate cephalopod song reader: distributed local intelligence over one exact waveform body."""
+
+from .model import *
+from .body import *
+from .personas import *
+from .arms import *
+from .render import *
+from .visualize import *
+from .nervous_system import *

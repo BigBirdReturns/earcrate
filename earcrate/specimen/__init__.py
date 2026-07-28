b0e08cc0a4ef23aaa65a09b37bafda67774cd291
BigBirdReturns <bigbirdreturns@proton.me>
2026-07-28 09:27:03 -0700

    Export community-symbolic specimen authority

--- earcrate/specimen/__init__.py
+++ earcrate/specimen/__init__.py
@@ -5,11 +5,27 @@ from .children import (
     children_load_bindings,
     children_load_builtin,
 )
+from .community import (
+    COMMUNITY_EVIDENCE_TIER,
+    COMMUNITY_PACK_RECEIPT_KIND,
+    COMMUNITY_REPORT_KIND,
+    community_bind_pack,
+    community_validate_report,
+    community_zip_inventory,
+)
 from .continuation_dense import (
     CHILDREN_CONTINUATION_BARS,
     CHILDREN_CONTINUATION_KIND,
     CHILDREN_CONTINUATION_SCHEMA_VERSION,
     children_compose_adjacent_move,
+)
+from .flim import (
+    FLIM_PROOF_PACK_SHA256,
+    FLIM_REQUIRED_PACK_MEMBERS,
+    FLIM_SPECIMEN_ID,
+    flim_bind_proof_pack,
+    flim_capability,
+    flim_load_builtin,
 )
 from .cli import specimen_capability, specimen_cli_main
 from .convergence import specimen_compare_score_audio, specimen_nearest_note_pairs
@@ -22,10 +38,22 @@ __all__ = [
     "children_compile_score_branch",
     "children_load_bindings",
     "children_load_builtin",
+    "COMMUNITY_EVIDENCE_TIER",
+    "COMMUNITY_PACK_RECEIPT_KIND",
+    "COMMUNITY_REPORT_KIND",
+    "community_bind_pack",
+    "community_validate_report",
+    "community_zip_inventory",
     "CHILDREN_CONTINUATION_BARS",
     "CHILDREN_CONTINUATION_KIND",
     "CHILDREN_CONTINUATION_SCHEMA_VERSION",
     "children_compose_adjacent_move",
+    "FLIM_PROOF_PACK_SHA256",
+    "FLIM_REQUIRED_PACK_MEMBERS",
+    "FLIM_SPECIMEN_ID",
+    "flim_bind_proof_pack",
+    "flim_capability",
+    "flim_load_builtin",
     "specimen_capability",
     "specimen_cli_main",
     "specimen_compare_score_audio",

7357fe3eb143b5c30c24bf26acc1c9ec401aa420
BigBirdReturns <bigbirdreturns@proton.me>
2026-08-01 11:57:26 -0700

    Expose Homelab operational authorities

--- earcrate/estate/__init__.py
+++ earcrate/estate/__init__.py
@@ -11,6 +11,14 @@ from earcrate.estate.homelab import (
     record_homelab_audition,
     record_homelab_stage,
 )
+from earcrate.estate.homelab_ops import (
+    backup_homelab_store,
+    export_public_store,
+    render_homelab_dashboard,
+    restore_homelab_backup,
+)
+from earcrate.estate.homelab_review import adjudicate_review, prepare_blind_review, record_review_submission
+from earcrate.estate.homelab_store import HomelabStore
 from earcrate.estate.model import default_estate_policy, estate_architecture
 from earcrate.estate.plan import apply_estate_plan, propose_estate_plan, rollback_estate_apply, verify_estate_apply
 from earcrate.estate.rig import capture_rig_capabilities, propose_local_acceptance_campaign
@@ -34,4 +42,12 @@ __all__ = [
     "record_homelab_audition",
     "decide_homelab_target",
     "homelab_sweep",
+    "HomelabStore",
+    "prepare_blind_review",
+    "record_review_submission",
+    "adjudicate_review",
+    "export_public_store",
+    "backup_homelab_store",
+    "restore_homelab_backup",
+    "render_homelab_dashboard",
 ]
